c6d106932e4b9de8031827ae6682b6975f1fe2aa
mirko cellino <mirkocellino@outlook.it>
2017-11-10 14:26:50 +0100

    commitfeat2

--- Git/Program.cs
+++ Git/Program.cs
@@ -6,9 +6,7 @@ namespace Git
     {
         static void Main()
         {
-            Console.WriteLine("Commit punto 2");
-            Console.WriteLine("Commit punto 3");
-            Console.ReadLine();
+            Console.WriteLine("Ciao Mondo!");
         }
     }
 }
